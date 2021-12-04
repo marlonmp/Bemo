6d7e1c77fcb3a0fe37ebc72e549919aa17520fbc
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2021-12-03 20:32:26 -0500

    listener library updated

--- src/lib/listener.py
+++ src/lib/listener.py
@@ -1,43 +1,73 @@
 class Listener:
 
+
     def __init__(self):
         self._events = {}   
 
-
-    def dispatch(self, event: str):
-
-        """This method dispatches every callbakcs with the index `event`"""
+    def _default_event(self, event: dict):
         
-        if event in self._events:
+        if not 'cancelable' in event:
+            event['cancelable'] = True
+        
+        if not 'content' in event:
+            event['content'] = {}
 
-            if isinstance(self._events[event], list):
 
-                for callback in self._events[event]:
-                    callback()
+    def _default_event_listener(self, event_type: str):
+
+        if not event_type in self._events:
+            self._events[event_type] = []
 
 
-    def add(self, event: str, *callbacks: callable):
+    def dispatch(self, event: dict):
+
+        """This method dispatches every callbakcs with the index of the event type (event name)
+
+        ```python
+        event = {
+            'type': str,
+            'cancelable': bool,
+            'content': dict
+        }
+        ```
+        """
+
+        event_type = event['type']
+
+        self._default_event_listener(event_type)
+
+        self._default_event(event)
+
+        if event_type in self._events:
+
+            if isinstance(self._events[event_type], list):
+
+                for callback in self._events[event_type]:
+                    continue_ = callback(event)
+
+                    if event['cancelable'] and continue_ == False: return
+
+
+    def add(self, event_type: str, *callbacks: callable):
 
         """This method add every events callbacks passed as `*callbacks`"""
 
+        self._default_event_listener(event_type)
+
         for callback in callbacks:
             
-            if event in self._events:
-                self._events[event].append(callback)
-
-            else:
-                self._events[event] = [callback]
+            self._events[event_type].append(callback)
 
 
-    def remove(self, event: str, *callbacks: callable):
+    def remove(self, event_type: str, *callbacks: callable):
 
         """This method remove every events callbacks passed as `*callbacks`"""
+
+        self._default_event_listener(event_type)
         
         for callback in callbacks:
+        
+            if callback in self._events[event_type]:
+                index = self._events[event_type].index(callback)
 
-            if event in self._events:
-
-                if callback in self._events[event]:
-                    index = self._events[event].index(callback)
-
-                    self._events[event].pop(index)
+                self._events[event_type].pop(index)
--- test/test_listener.py
+++ test/test_listener.py
@@ -7,41 +7,57 @@ class test_Listener(unittest.TestCase):
     def setUp(self):
         self.listener = Listener()
 
-        self.msg = '[EVENT] starting'
+        self.start_event = {
+            'type': 'start',
+            'content': {
+                'num_1': 2,
+                'num_2': 4,
+                'cancel': True
+            }
+        }
 
         self.num = 0
 
-        def on_start_1():
-            print(self.msg)
+        def on_start_1(event: dict):
+            print('\n\nEvent:',event, '\n\n')
+            self.num += event['content']['num_1']
 
-        def on_start_2():
-            self.num += 2
-
-        def on_start_3():
-            print('[EVENT_MSG] ', self.msg)
+            if event['content']['cancel']:
+                return False
             
-        def on_start_4():
-            self.num += 4
+        def on_start_2(event: dict):
+            self.num += event['content']['num_2']
 
-        self.funcs = [on_start_1, on_start_2]
+        self.listener_to_remove = on_start_1
             
 
-        self.listener.add('start', on_start_1, on_start_2, on_start_1, on_start_3, on_start_4, on_start_1)
+        self.listener.add('start', on_start_1, on_start_2, on_start_1)
 
         print('listener events:', self.listener._events)
 
 
-    def test_dispatch_event(self):
+    def test_dispatch_event_cancel(self):
 
-        self.listener.dispatch('start')
+        self.listener.dispatch(self.start_event)
 
-        self.assertEqual(self.num, 6)
+        self.assertEqual(self.num, 2)
+
+
+    def test_dispatch_event_no_cancel(self):
+
+        self.start_event['content']['cancel'] = False
+
+        self.listener.dispatch(self.start_event)
+
+        self.assertEqual(self.num, 8)
 
 
     def test_remove_listener(self):
+
+        self.start_event['content']['cancel'] = False
         
-        self.listener.remove('start', *self.funcs)
+        self.listener.remove('start', self.listener_to_remove)
 
-        self.listener.dispatch('start')
+        self.listener.dispatch(self.start_event)
 
-        self.assertEqual(self.num, 4)
+        self.assertEqual(self.num, 6)

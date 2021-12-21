b7a2de3907c1d03b4a28309172fd9e07c91c510e
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2021-12-21 17:14:32 -0500

    The song class was added

--- src/model/song.py
+++ src/model/song.py
@@ -1,46 +1,94 @@
-import re
-
-from ..lib.video_info import get
-from ..lib.time_format import format
+from src.lib import time_format
+from src.lib import video_info
 
 class Song:
 
-    id: str
-    title: str
-    duration: int
+    __id: int
+    __video_id: str
+    __title: str
+    __duration: int
 
-    def __init__(self, url='', dict={}):
+## constructor
 
-        if url != '':
-            self.__get_from_url(url)
-
-        elif dict != {}:
-            self.__get_from_dict(dict)
-
-    def __get_from_url(self, url: str):
-
-        video= get(url)
-
-        self.id = video['id']
-        self.title = video['title']
-
-        # time in seconds
-        self.duration = video['duration']
-
-    def __get_from_dict(self, dict: dict):
+    def __init__(self, *, set_from_db: dict = {}, set_from_url: str = ''):
         
-        self.id = dict['id']
-        self.title = dict['title']
-        self.duration = dict['duration']
+        self.__id = 0
 
-    def get_duration_formated(self):
-        return format(self.duration)
+        if set_from_db: self.__build_from_db(set_from_db)
 
-    def to_dict(self) -> dict:
-        
+        elif set_from_url: self.__build_from_url(set_from_url)
+
+## build functions
+
+    def __build_from_db(self, dict_: dict):
+
+        self.__id = dict_['id']
+
+        self.__video_id = dict_['video_id']
+
+        self.__title = dict_['title']
+
+        self.__duration = dict_['duration']
+
+    
+    def __build_from_url(self, url: str):
+
+        info = video_info.get(url)
+
+        self.__video_id = info['id']
+
+        self.__title = info['title']
+
+        self.__duration = info['duration']
+
+## setters
+
+    def set_id(self, id: int):
+        self.__id = id
+
+## getters
+
+    def get_id(self):
+        return self.__id
+
+
+    def get_video_id(self):
+        return self.__video_id
+
+
+    def get_title(self):
+        return self.__title
+
+
+    def get_cover_url(self):
+        return 'https://i3.ytimg.com/vi/' + self.__video_id + '/maxresdefault.jpg'
+
+
+    def get_video_url(self):
+        return 'https://youtu.be/' + self.__video_id
+
+
+    def get_duration(self):
+        return self.__duration
+
+
+    def get_duration_fmt(self):
+        return time_format.format(self.__duration)
+
+
+    def get_source(self):
+
+        info = video_info.get(self.get_video_url())
+
+        return info['formats'][0]['url']
+
+## fmt
+
+    def to_dict(self):
+
         return {
-            'id': self.id,
-            'title': self.title,
-            'duration': self.duration
+            'id': self.__id,
+            'video_id': self.__video_id,
+            'title': self.__title,
+            'duration': self.__duration
         }
-        

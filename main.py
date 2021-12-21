4fb41240d40a4c32aa1f029b39327805af245e49
Marlon Mosquera Pareja <marlonppp214@protonmail.com>
2021-12-21 17:24:58 -0500

    main file

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-from discord.voice_client import VoiceClient as Conn
-from discord.ext.commands.context import Context
-from discord import VoiceChannel, utils, FFmpegPCMAudio
-from discord.ext import commands
+from logging import error
+from dotenv import load_dotenv
+from src.client import Client
+import os
 
-from youtube_dl import YoutubeDL
+if __name__ == '__main__':
 
-client = commands.Bot(command_prefix='!')
+    load_dotenv()
 
-def get(url: str):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'noplaylist': 'True',
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192'
-            }
-        ]
-    }
+    TOKEN = os.environ.get('TOKEN')
 
-    with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        return (info, info['formats'][0]['url'])
+    if TOKEN is None: raise error('[ERROR] Error getting token')
+    
+    client = Client()
 
-@client.command()
-async def play(ctx: Context, url: str):
-
-    voice = utils.get(client.voice_clients, guild=ctx.guild)
-    voiceChan: VoiceChannel = utils.get(ctx.guild.voice_channels, name='General')
-
-    conn: Conn = await voiceChan.connect()
-
-    FFMPEG_OPTS = { 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn' }
-
-    video, source = get(url)
-    conn.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
-    conn.is_playing()
-
-client.run('')
+    client.run(TOKEN)
